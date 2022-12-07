30cd24c08a8b292f060fabc1be28427b8391bee8
Denis Sychev <sychev@peterpartner.net>
2022-12-07 21:15:03 +0600

    rename Translitor.java to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.io.IOException;
-
 //script url link = https://script.google.com/macros/s/AKfycbwi_0PoxFRAyv3DZynvdcfiYiaUzrO3ktaM6BD2Wm1FjqhORXStNEAv20WN_OBUTWpbXw/exec
 
-public class Translitor {
+public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String text = "Hello world!";
         //Translated text: Hallo Welt!
         System.out.println("Translated text: " + TextTranslator.execute(text));
